b1d0f3fc3c9815fe5a3bf0a9147ea31636074173
Matheus Esteves <mat.esteves@gmail.com>
2025-04-10 02:10:47 -0300

    example logger function

---
+++ logger.py
@@ -0,0 +1,13 @@
+#decorador basicamente adiciona ou altera a funçao original, nesse caso ele está adicionando logs informativos para garantir que esta tudo funcionando da maneira correta. Percebe-se que na linha 7 é onde a função roda.
+def log_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.info(f"Chamando função '{func.__name__}' com args {args} e kwargs {kwargs}")
+        try:
+            result = func(*args, **kwargs)
+            logger.info(f"Função '{func.__name__}' retornou {result}")
+            return result
+        except Exception as e:
+            logger.exception(f"Exceção capturada em '{func.__name__}': {e}")
+            raise  # Re-lança a exceção para não alterar o comportamento da função decorada
+    return wrapper
